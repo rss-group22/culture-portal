889c83fba99a474851f5e17dd68d8ff261600859
MaxDaniLOVE <mahseeen@gmail.com>
2020-02-17 16:44:08 +0300

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## culture-portal
+
+To install dependencies:
+`npm i` 
+
+To start dev-server on http://localhost:8080 (auto-open enabled):
+`npm start` 
+
+To build project:
+`npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader' 
+      },
+      {
+        test: /\.(png|gif|jpg|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/, 
+        use: [
+          MiniCssExtractPlugin.loader ,
+          'css-loader' ,
+        ] 
+      },
+      {
+        test: /\.s[ca]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader ,
+          'css-loader' ,
+          'sass-loader' ,
+        ] 
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-[hash].css'
+    })
+  ],
+  devServer: {
+    open: true
+  }
+};
