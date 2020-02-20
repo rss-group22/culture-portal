06cf799559e1353936d6c90832bc9ce6e18c8a96
Minihilator <minihilator@gmail.com>
2020-02-20 14:57:27 +0300

    fix: add editorconfig and fix errors with eslint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import StyleGuide from '../StyleGuide';
 
 export default class App extends Component {
--- src/components/StyleGuide/ColorsTab.js
+++ src/components/StyleGuide/ColorsTab.js
@@ -1,66 +1,42 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-const colors1 = ["#ffffff", "#f8f9fa", "#eeeeee", "#dee2e6", "#ced4da", "#aaaaaa", "#777777", "#495057", "#333333", "#222222", "#000000"];
-const colors2 = ["#EB6864", "#6610f2", "#6f42c1", "#e83e8c", "#F57A00", "#fd7e14", "#F5E625", "#22B24C", "#20c997", "#369369"];
+const colors1 = ['#ffffff', '#f8f9fa', '#eeeeee', '#dee2e6', '#ced4da', '#aaaaaa', '#777777', '#495057', '#333333', '#222222', '#000000'];
+const colors2 = ['#EB6864', '#6610f2', '#6f42c1', '#e83e8c', '#F57A00', '#fd7e14', '#F5E625', '#22B24C', '#20c997', '#369369'];
 
-export default function ColorsTab () {
-    return (
-        <Fragment>
-            <h2 className="subtitle text-center">We are used the next colors</h2>
-            <div className="d-flex flex-wrap justify-content-center">
-                {
-                    colors1.map((color, index) => {
-                        return (
-                            <div className="color-block" key={`color-${index}`}>
-                                <div className="color" style={{background: color}}></div>
-                                <p className="text-secondary text-center"><small>{color}</small></p>
-                            </div>
-                        );
-                    })
-                }
+export default function ColorsTab() {
+  return (
+    <>
+      <h2 className="subtitle text-center">We are used the next colors</h2>
+      <div className="d-flex flex-wrap justify-content-center">
+        {
+          colors1.map((color) => (
+            <div className="color-block" key={`color-${color}`}>
+              <div className="color" style={{ background: color }} />
+              <p className="text-secondary text-center"><small>{color}</small></p>
             </div>
-            <div className="d-flex flex-wrap justify-content-center">
-                {
-                    colors2.map((color, index) => {
-                        return (
-                            <div className="color-block" key={`color-${index}`}>
-                                <div className="color" style={{background: color}}></div>
-                                <p className="text-secondary text-center"><small>{color}</small></p>
-                            </div>
-                        );
-                    })
-                }
+          ))
+        }
+      </div>
+      <div className="d-flex flex-wrap justify-content-center">
+        {
+          colors2.map((color) => (
+            <div className="color-block" key={`color-${color}`}>
+              <div className="color" style={{ background: color }} />
+              <p className="text-secondary text-center"><small>{color}</small></p>
             </div>
-            <div className="jumbotron">
-                <h2 className="subtitle text-center">Also you can use this colors like scss variables</h2>
-                <pre><code className="language-css">
-{
-`
-$white:    #fff;
-$gray-100: #f8f9fa;
-$gray-200: #eee;
-$gray-300: #dee2e6;
-$gray-400: #ced4da;
-$gray-500: #aaa;
-$gray-600: #777;
-$gray-700: #495057;
-$gray-800: #333;
-$gray-900: #222;
-$black:    #000;
-
-$blue:    #EB6864;
-$indigo:  #6610f2;
-$purple:  #6f42c1;
-$pink:    #e83e8c;
-$red:     #F57A00;
-$orange:  #fd7e14;
-$yellow:  #F5E625;
-$green:   #22B24C;
-$teal:    #20c997;
-$cyan:    #369369;`
-}
-                </code></pre>
-            </div>
-        </Fragment>
-    );
+          ))
+        }
+      </div>
+      <div className="jumbotron">
+        <h2 className="subtitle text-center">Also you can use this colors like scss variables</h2>
+        <pre>
+          <code className="language-css">
+            {
+              `$white: #fff;\n$gray-100: #f8f9fa;\n$gray-200: #eee;\n$gray-300: #dee2e6;\n$gray-400: #ced4da;\n$gray-500: #aaa;\n$gray-600: #777;\n$gray-700: #495057;\n$gray-800: #333;\n$gray-900: #222;\n$black: #000;\n\n$blue: #EB6864;\n$indigo: #6610f2;\n$purple: #6f42c1;\n$pink: #e83e8c;\n$red: #F57A00;\n$orange: #fd7e14;\n$yellow:  #F5E625;\n$green: #22B24C;\n$teal: #20c997;\n$cyan: #369369;`
+            }
+          </code>
+        </pre>
+      </div>
+    </>
+  );
 }
