5fd1fcef505beb7b5531fe7db00288b77c4aead3
MaxDaniLOVE <mahseeen@gmail.com>
2020-02-21 12:29:32 +0300

    feat: add styles for TimelineComponent

--- src/components/TimelineComponent/TimelineComponent.js
+++ src/components/TimelineComponent/TimelineComponent.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
+import './TimelineComponent.scss';
 
-const TimelineComponent = () => {
-  const biographyTimeline = [
+const TimelineComponent = ({ biographyTimeline }) => {
+  const biographyTimelineTest = [
     {
       year: '1965 - 1985',
       label: 'Учился в детской народной изостудии при Дворце культуры текстильщиков в Минске.',
@@ -21,9 +23,9 @@ const TimelineComponent = () => {
     },
   ];
 
-  const timelineItems = biographyTimeline.map(({ year, label }) => (
+  const timelineItems = biographyTimelineTest.map(({ year, label }, idx) => (
     <TimelineItem
-      key="001"
+      key={idx + 1}
       dateText={year}
       className="timeline-years"
     >
