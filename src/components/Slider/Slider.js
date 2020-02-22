7a2dffe9bb7e7dbe62bcc36cc42e183a5aec1d2e
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-02-22 16:13:49 +0300

    fix: fixstyles for slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -5,32 +5,31 @@ import './Slider.scss';
 
 const Slider = ({photoGallery = []}) => {
   const params = {
-    effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true
-      },
-      pagination: {
-        el: '.swiper-pagination'
-      },
-      shouldSwiperUpdate: true
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+    shouldSwiperUpdate: true,
   }
   return (
-    <Swiper {...params}>
-      {photoGallery.map((el) => {
-        return (
-          <div className="image-wrapper" key={el}>
-            <img className="work-image" src={el} alt='works' height="100" />
-          </div>
-        )
-      })}
-    </Swiper>
+    <div className="slider-wrapper">
+      <Swiper {...params}>
+        {photoGallery.map((el) => {
+          return (
+            <div className="image-wrapper" key={el}>
+              <img className="work-image" src={el} alt='works' height="100" />
+            </div>
+          )
+        })}
+      </Swiper>
+    </div>
   );
 }
 
