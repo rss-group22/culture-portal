c318570643b0e64b0105da3aa2b579b1f523b59c
Liudmila Arzamasova <liudmila.arzamasova@gmail.com>
2020-02-25 16:25:25 +0300

    feat: add styles of worklog

--- src/components/Team/Worklog/Worklog.js
+++ src/components/Team/Worklog/Worklog.js
@@ -1,77 +1,43 @@
 import React, { Component } from "react";
+import shortid from "shortid";
 import dataWorklog, { headersTable } from "../../../data/dataWorklog";
+
+import "./Worklog.scss";
 
 export default class Worklog extends Component {
   renderTableHandle() {
     return headersTable.map(header => {
-      return <th key={header}>{header}</th>;
+      return <th key={shortid.generate()}>{header}</th>
     });
   }
 
   render() {
     return (
-      <>
+      <div className="container">
         <h1 className="worklog__title">Worklog</h1>
-        {dataWorklog.map((row) => {
-          const key = 0;
-          const {
-            name,
-            surname,
-            time1,
-            feature1,
-            time2,
-            feature2,
-            time3,
-            feature3,
-            time4,
-            feature4,
-            time5,
-            feature5,
-            time6,
-            feature6,
-            time7,
-            feature7
-          } = row;
+        {dataWorklog.map(row => {
           return (
             <>
+              <h4 key={shortid.generate()} className="worklog__user">
+                {`${row.name} ${row.surname}`}
+              </h4>
               <table className="worklog__table">
+                <tr className="worklog__header">{this.renderTableHandle()}</tr>
                 <tbody>
-                  <h4>{`${name} ${surname}`}</h4>
-                  <tr>{this.renderTableHandle()}</tr>
-                  <tr key={key + 1}>
-                    <td>{time1}</td>
-                    <td>{feature1}</td>
-                  </tr>
-                  <tr key={key + 1}>
-                    <td>{time2}</td>
-                    <td>{feature2}</td>
-                  </tr>
-                  <tr key={key + 1}>
-                    <td>{time3}</td>
-                    <td>{feature3}</td>
-                  </tr>
-                  <tr key={key + 1}>
-                    <td>{time4}</td>
-                    <td>{feature4}</td>
-                  </tr>
-                  <tr key={key + 1}>
-                    <td>{time5}</td>
-                    <td>{feature5}</td>
-                  </tr>
-                  <tr key={key + 1}>
-                    <td>{time6}</td>
-                    <td>{feature6}</td>
-                  </tr>
-                  <tr key={key + 1}>
-                    <td>{time7}</td>
-                    <td>{feature7}</td>
-                  </tr>
+                  {row.done.map(elem => {
+                    return (
+                      <tr className="worklog__rows">
+                        <td>{`${elem.time} h`}</td>
+                        <td>{elem.feature}</td>
+                      </tr>
+                    );
+                  })}
                 </tbody>
               </table>
             </>
           );
         })}
-      </>
+      </div>
     );
   }
 }
