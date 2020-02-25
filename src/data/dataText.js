7d6b44b56f774f052949bce180fea424550a00d1
Minihilator <minihilator@gmail.com>
2020-02-26 00:24:55 +0300

    feat: add real info to the home page

--- src/data/dataText.js
+++ src/data/dataText.js
@@ -8,7 +8,17 @@ const dataText = {
       nav4: 'Search photographers'
     },
     Home: {
-      title: 'Home',
+      title: 'Photographers of Belarus',
+      aboutPortal: {
+        title: 'About portal',
+        text: [
+          "Photo art originates in 1839, when the first daguerreotype was made in Paris.",
+          "And already in the 21st century on our portal you can choose a photographer to your taste, get acquainted with his work, decide on the format of photography and its location.",
+          "Do you want to find in our catalog the best artist of your dreams about the most beautiful photo shoot, which will correspond not only to your ideas about the style, but also fit into the budget? Then choose it based on the quality of photos, real customer reviews and service prices. To do this, we tried to make the most convenient search, with the ability to sort by price, reviews and cities.",
+          "We also advise you to pay attention to the “photographer of the day” section, it will be useful for beginners of our site and not only  In it, according to the majority of visitors to the site, the best photographer and his work are determined every day.",
+          "Evaluate, enjoy, choose and capture the best moments of your life!"
+        ]
+      }
     },
     Person: {
       title: 'Photographer of the day',
@@ -32,7 +42,17 @@ const dataText = {
       nav4: 'Найти фотографа'
     },
     Home: {
-      title: 'Домашняя страница',
+      title: 'Фотографы Беларуси',
+      aboutPortal: {
+        title: 'О портале',
+        text: [
+          "Фотоискусство берет свое начало в 1839 году, когда в Париже был сделан первый дагерротип.",
+          "А уже в 21 веке на нашем портале Вы сможете подобрать фотографа на Ваш вкус, ознакомиться с его работами, определиться с форматом фотосъемки и ее местом проведения.",
+          "Хотите найти в нашем каталоге лучшего исполнителя Вашей мечты о самой прекрасной фотосессии, который будет соответствовать не только Вашим представлениям о стиле, но и впишется в бюджет? Тогда выбирайте его на основе качества фотографий, реальных отзывов от клиентов и цен на услуги. Для этого мы постарались сделать максимально удобный поиск, с возможностью сортировки по ценам, отзывам и городам.",
+          "Также советуем обратить внимание на рубрику «фотограф дня», она будет полезна для новичков нашего сайта и не только J  В ней каждый день по мнению большинства посетителей  сайта определяется лучший фотограф и его работы.",
+          "Оценивайте, наслаждайтесь, выбирайте и запечатлейте лучшие моменты своей жизни!"
+        ]
+      }
     },
     Person: {
       title: 'Фотограф дня',
@@ -56,7 +76,17 @@ const dataText = {
       nav4: 'Знайсці фатографа'
     },
     Home: {
-      title: 'Хатняя старонка',
+      title: 'Фатографы Беларусі',
+      aboutPortal: {
+        title: 'Аб партале',
+        text: [
+          "Фотамастацтва бярэ свой пачатак у 1839 годзе, калі ў Парыжы быў зроблены першы дагерротип.",
+          "А ўжо ў 21 стагоддзі на нашым партале вы зможаце падабраць фатографа на Ваш густ, азнаёміцца з яго працамі, вызначыцца з фарматам фотаздымкі і яе месцам правядзення.",
+          "Хочаце знайсці ў нашым каталогу лепшага выканаўцы вашай мары аб самай выдатнай фотасесіі, які будзе адпавядаць не толькі вашым уяўленням аб стылі, але і ўпішацца ў бюджэт? Тады выбірайце яго на аснове якасці фатаграфій, рэальных водгукаў ад кліентаў і цэн на паслугі. Для гэтага мы пастараліся зрабіць максімальна зручны пошук, з магчымасцю сартавання па цэнах, водгуках і гарадах.",
+          "Таксама раім звярнуць увагу на рубрыку \"фатограф дня\", яна будзе карысная для пачаткоўцаў нашага сайта і не толькі ў ёй кожны дзень на думку большасці наведвальнікаў сайта вызначаецца лепшы фатограф і яго працы.",
+          "Ацэньвайце, атрымлівайце асалоду ад, выбірайце і захаваць лепшыя моманты свайго жыцця!"
+        ]
+      }
     },
     Person: {
       title: 'Фатограф дня',
