6d3125aea4d87a7198dea4a774bc2c6da3cd7deb
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-02-29 15:27:19 +0300

    feat: add production version

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
